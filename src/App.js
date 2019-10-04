2a87ff22c2d40ca7e02935141fed0c39a3daf825
Akhil <akhil9tiet@gmail.com>
2019-10-04 00:07:31 -0700

    wip: component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import NavBar from "./Components/NavBar";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			<p> BODY</p>
+			<p> FOOTER</p>
+		</div>
+	);
 }
 
 export default App;
