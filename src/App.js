ea711f3cd27096bb825787f1ebd9becc0d487c9f
Akhil <akhil9tiet@gmail.com>
2019-10-06 21:34:20 -0700

    wip: introducing content for navbar and dropdown

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from "react";
-import NavBar from "./Components/NavBar";
-import "./App.css";
+import React from 'react';
+import NavBar from './Components/NavBar';
+import './App.css';
 
 function App() {
 	return (
 		<div>
 			<NavBar />
 			<p> BODY</p>
+			<p>This is the best dat </p>
 			<p> FOOTER</p>
 		</div>
 	);
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,6 +1,38 @@
-import React from 'react';
+import React, * as react from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const Dropdown = () => {
+	return (
+		<Grid container spacing={3}>
+			<Grid item xs={1} md={3}>
+				<Paper>xs=3</Paper>
+			</Grid>
+			<Grid item xs={1} md={3}>
+				<Paper>xs=3</Paper>
+			</Grid>
+			<Grid item xs={1} md={3}>
+				<Paper>xs=3</Paper>
+			</Grid>
+			<Grid item xs={1} md={3}>
+				<Paper>xs=3</Paper>
+			</Grid>
+		</Grid>
+	);
+};
 
 const NavBar = () => {
-  return (<h1>NavBar</h1>);
-}
+	return (
+		<ul>
+			<li>Projects</li>
+			<div>
+				<Dropdown />
+			</div>
+			<li>Blog</li>
+			<li>Skills</li>
+			<li>Story</li>
+		</ul>
+	);
+};
 export default NavBar;
