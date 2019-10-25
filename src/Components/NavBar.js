a0dbf4b2ac426a077921c5b00876f3c13147e94f
Akhil <akhil9tiet@gmail.com>
2019-10-25 02:28:59 -0700

    task: all content in

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,18 +5,43 @@ import Grid from '@material-ui/core/Grid';
 
 const Dropdown = () => {
 	return (
-		<Grid container spacing={3}>
-			<Grid item xs={1} md={3}>
-				<Paper>xs=3</Paper>
+		<Grid container>
+			<Grid item xs={12} sm={2}>
+				{''}
 			</Grid>
-			<Grid item xs={1} md={3}>
-				<Paper>xs=3</Paper>
+			<Grid item xs={12} sm={2}>
+				<h4>Web Dev</h4>
+				<ul>
+					<li>D3 World Map</li>
+					<li>Keppler</li>
+					<li>P5.js</li>
+					<li>Gatsby</li>
+				</ul>
 			</Grid>
-			<Grid item xs={1} md={3}>
-				<Paper>xs=3</Paper>
+			<Grid item xs={12} sm={2}>
+				<h4>Data Science</h4>
+				<ul>
+					<li>Data Mining</li>
+					<li>Django web app</li>
+					<li>Kaggle</li>
+					<li>US Phone Complaints</li>
+				</ul>
 			</Grid>
-			<Grid item xs={1} md={3}>
-				<Paper>xs=3</Paper>
+			<Grid item xs={12} sm={2}>
+				<h4>Design</h4>
+				<ul>
+					<li>Posters</li>
+					<li>Sketch</li>
+				</ul>
+			</Grid>
+			<Grid item xs={12} sm={2}>
+				<h4>Video</h4>
+				<ul>
+					<li>Youtube Vlog</li>
+				</ul>
+			</Grid>
+			<Grid item xs={12} sm={2}>
+				<h4>Misc</h4>
 			</Grid>
 		</Grid>
 	);
