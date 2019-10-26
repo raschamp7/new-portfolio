14af8d73b3332abed95b91925ae31ee2865ea57e
Akhil <akhil9tiet@gmail.com>
2019-10-26 15:20:26 -0700

    feat: adding react routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import NavBar from './Components/NavBar';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Blog from './components/Blog';
+import Skills from './components/Skills';
+import Story from './components/Story';
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
 	return (
 		<div>
 			<NavBar />
-			<p> BODY</p>
-			<p>This is the best dat </p>
+			<Switch>
+				<Route exact={true} path="/" component={Home} />
+				<Route path="/home" component={Home} />
+				<Route exact path="/blog" component={Blog} />
+				<Route exact path="/skills" component={Skills} />
+				<Route exact path="/story" component={Story} />
+			</Switch>
+
 			<p> FOOTER</p>
 		</div>
 	);
---
+++ src/Components/Story.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Story = () => {
+	return <h1>Story</h1>;
+};
+
+export default Story;
