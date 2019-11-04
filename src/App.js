2aa23874ce06ee5a4cc74a0423d4015d4856dea7
Akhil <akhil9tiet@gmail.com>
2019-11-03 21:25:36 -0800

    wip: add tfjs component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Blog from './components/Blog';
 import Skills from './components/Skills';
 import Story from './components/Story';
 import D3WorldMap from './components/D3WorldMap';
+import TensorflowJS from './components/TensorflowJS';
 import NavBar from './components/NavBar';
 import './App.css';
 
@@ -18,6 +19,7 @@ function App() {
 				<Route exact path="/blog" component={Blog} />
 				<Route exact path="/skills" component={Skills} />
 				<Route exact path="/d3-world-map" component={D3WorldMap} />
+				<Route exact path="/tensorflow-js" component={TensorflowJS} />
 				<Route exact path="/story" component={Story} />
 			</Switch>
 
---
+++ src/Components/TensorflowJS/TensorflowJS.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TensorflowJS = () => {
+	return (
+		<div>
+			<p>Skol</p>
+		</div>
+	);
+};
