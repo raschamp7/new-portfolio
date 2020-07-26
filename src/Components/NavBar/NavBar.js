788cc7edc773e2f0ee25d1787859584cf0d57994
raschamp7 <33166653+raschamp7@users.noreply.github.com>
2020-07-26 12:38:46 -0400

    working mobile nav, issues present with click away

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -24,8 +24,6 @@ const Dropdown = ({ callbackFromParent }) => {
 
 	return (
 		<Grid container ref={node}>
-			<Grid item xs={12} sm={4} md={4}>
-				<h4>Web Dev</h4>
 				<ListItem onClick={() => callbackFromParent(false)}>
 					<Link to="/d3-world-map">D3 World Map</Link>
 				</ListItem>
@@ -33,20 +31,7 @@ const Dropdown = ({ callbackFromParent }) => {
 				<ListItem>P5.js</ListItem>
 				<ListItem>Gatsby</ListItem>
 				<ListItem>GraphQL</ListItem>
-			</Grid>
-			<Grid item xs={12} sm={4} md={4}>
-				<h4>Data Science</h4>
-				<ListItem>Data mining</ListItem>
-				<ListItem>Big Data and AI</ListItem>
-				<ListItem>Django web app</ListItem>
-				<ListItem>Kaggle Challenge</ListItem>
-			</Grid>
-			<Grid item xs={12} sm={4} md={4}>
-				<h4>Design</h4>
-				<ListItem>Posters</ListItem>
-				<ListItem>Sketch</ListItem>
-				<ListItem>Videos</ListItem>
-			</Grid>
+			
 		</Grid>
 	);
 };
@@ -60,24 +45,10 @@ const NavBar = () => {
 
 	return (
 		<Grid container className="navbar">
-			<Grid item xs={12} sm={12} md={4} lg={4}>
-				<Link to="/">
-					<img className="logo" src={process.env.PUBLIC_URL + '/logo.png'} alt="Logo" />
-				</Link>
-			</Grid>
-			<Grid item xs={12} sm={12} md={8} lg={8}></Grid>
-			<ul>
-				<li onClick={() => setListOpen(!listOpen)}>Projects</li>
-				<li onClick={() => setListOpen(false)}>
-					<Link to="/blog">Blog</Link>
-				</li>
-				<li onClick={() => setListOpen(false)}>
-					<Link to="/skills">Skills</Link>
-				</li>
-				<li onClick={() => setListOpen(false)}>
-					<Link to="/story">Story</Link>
-				</li>
-			</ul>
+			<div>
+			<li className="topstrip" onClick={() => setListOpen(!listOpen)}><i class="fa fa-bars"></i></li>
+			<a href="/" className="name">The Kindness Club</a>
+			</div>
 			<div className="dropdown">{listOpen ? <Dropdown callbackFromParent={myCallback} /> : null}</div>
 		</Grid>
 	);
